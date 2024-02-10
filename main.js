dca8b038003f97450091b3018a57f211afe6b8f8
Bomback9 <129324082+Bomback9@users.noreply.github.com>
2024-02-10 14:47:58 -0800

    Fully finished and formatted code

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+//api Key with Current Date variable
 const apiKey = "a335ea562508ddcaaec55b37898a39bd";
 const currentDate = new Date().toDateString();
 
+//Funciton to fetch the API Data and return the needed Information
 const fetchWeatherData = async (city, state, apiKey) => {
   try {
+    //fetching the GeoLocater to get Coordinates
     const geoRes = await fetch(
       `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=5&appid=${apiKey}`
     );
@@ -10,24 +13,26 @@ const fetchWeatherData = async (city, state, apiKey) => {
     const lat = geoData[0].lat.toFixed(2);
     const long = geoData[0].lon.toFixed(2);
 
+    //fetching the WeatherAPI
     const weatherResponse = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&tz=-08:00&units=Imperial&appid=a335ea562508ddcaaec55b37898a39bd`
     );
     const weatherData = await weatherResponse.json();
+    const iconCode = weatherData.weather[0].icon;
 
     const temp = Math.floor(weatherData.main.temp);
+    const loc = weatherData.name;
+    const icon = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+    const condition = weatherData.weather[0].main;
     const wind = Math.floor(weatherData.wind.speed);
     const sunriseUtc = Math.floor(weatherData.sys.sunrise);
     const sunsetUtc = Math.floor(weatherData.sys.sunset);
-    const condition = weatherData.weather[0].main;
-    const timeZoneDif = weatherData.timezone;
-    const iconCode = weatherData.weather[0].icon;
-    const icon = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
+    //Timezone converted from UTC in seconds to whatever time zone the user inputs
+    const timeZoneDif = weatherData.timezone;
     const sunrise = convertTime(sunriseUtc, timeZoneDif);
     const sunset = convertTime(sunsetUtc, timeZoneDif);
-
-    const loc = weatherData.name;
 
     return { temp, wind, condition, sunrise, sunset, loc, icon };
   } catch (e) {
@@ -35,11 +40,10 @@ const fetchWeatherData = async (city, state, apiKey) => {
   }
 };
 
-// UTC time in seconds
+// UTC time converter
 const convertTime = (t, tD) => {
   const utcTimeInSeconds = t + tD;
 
-  // Create a new Date object using the UTC time in seconds
   const date = new Date(utcTimeInSeconds * 1000);
 
   // Extract individual time components
@@ -56,21 +60,20 @@ const convertTime = (t, tD) => {
     .toString()
     .padStart(2, "0")} ${meridiem}`;
 
-  // Output the formatted time
   return formattedTime;
 };
 
 const form = document.querySelector(".location");
-const weatherInfo = document.querySelector(".weatherFacts");
 const tempature = document.querySelector(".temperature");
 const place = document.querySelector(".place");
 const date = document.querySelector(".date");
 const ic = document.querySelector(".icon");
-const con = document.querySelector(".condition");
+const cond = document.querySelector(".condition");
 const breeze = document.querySelector(".wind");
 const sunup = document.querySelector(".sunrise");
 const sundown = document.querySelector(".sunset");
 
+//Search Protocol for city and state the user inputed
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -79,7 +82,7 @@ form.addEventListener("submit", (e) => {
 
   const city = cityInput.value;
   const state = stateInput.value;
-
+  //call the funtion to get the data and display it on the screen
   fetchWeatherData(city, state, apiKey).then(
     ({ temp, wind, condition, sunrise, sunset, loc, icon }) => {
       const img = document.createElement("img");
@@ -87,7 +90,7 @@ form.addEventListener("submit", (e) => {
       tempature.innerHTML = `${temp}°`;
       place.innerHTML = loc;
       ic.innerHTML = `<img src="${icon}" alt="">`;
-      con.innerHTML = `Condition: ${condition}`;
+      cond.innerHTML = `Condition: ${condition}`;
       breeze.innerHTML = `Wind: ${wind} mph`;
       sunup.innerHTML = `Sunrise: ${sunrise}`;
       sundown.innerHTML = `Sunset: ${sunset}`;
