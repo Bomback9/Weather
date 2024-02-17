8c557c0224ed251439e743c73924a9246058965d
Bomback9 <129324082+Bomback9@users.noreply.github.com>
2024-02-17 14:06:17 -0800

    achieved form data passing thru the page

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const { render } = require("ejs");
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true,
+  })
+);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -8,6 +18,16 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+app.get("/results", (req, res) => {
+  res.render("results");
+});
+
+app.post("/results", (req, res) => {
+  const { city, state } = req.body;
+  console.log(city);
+  console.log(state);
+  res.redirect("/results");
 });
 
 app.listen(3000, () => {
