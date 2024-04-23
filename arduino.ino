6512a8134125108b40c91cab191fa6217c60d53e
Bomback9 <129324082+Bomback9@users.noreply.github.com>
2024-04-23 03:01:14 -0700

    updated dependecies to take cat quotes

--- arduino.ino
+++ arduino.ino
@@ -1,10 +1,9 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <ArduinoJson.h> 
 
 const char* ssid = "";
 const char* password = "";
-const char* serverUrl = "http://192.168.86.33:3000/results"; 
+const char* serverUrl = "http://192.168.86.33:3000/";
 
 void setup() {
   Serial.begin(115200);
@@ -28,32 +27,13 @@ void loop() {
     int httpCode = http.GET();
 
     if (httpCode > 0) {
-      // If successful, parse JSON response
-      StaticJsonDocument<256> doc; // Create a JSON document
-
-      // Deserialize the JSON data
-      DeserializationError error = deserializeJson(doc, http.getString());
-
-      // Check for errors in parsing JSON
-      if (error) {
-        Serial.print("deserializeJson() failed: ");
-        Serial.println(error.f_str());
+      if (httpCode == HTTP_CODE_OK) {
+        // If successful, read the response
+        String catQuote = http.getString();
+        Serial.println("Cat Quote: " + catQuote);
       } else {
-        // Extract values from JSON
-        float temp = doc["temp"];
-        int wind = doc["wind"];
-        const char* condition = doc["condition"];
-        const char* sunrise = doc["sunrise"];
-        const char* sunset = doc["sunset"];
-        const char* loc = doc["loc"];
-
-        // Print extracted values
-        Serial.println("Temperature: " + String(temp));
-        Serial.println("Wind: " + String(wind));
-        Serial.println("Condition: " + String(condition));
-        Serial.println("Sunrise: " + String(sunrise));
-        Serial.println("Sunset: " + String(sunset));
-        Serial.println("Location: " + String(loc));
+        Serial.print("HTTP request failed with error code: ");
+        Serial.println(httpCode);
       }
     } else {
       Serial.println("Error on HTTP request");
@@ -62,5 +42,5 @@ void loop() {
     http.end();
   }
 
-  delay(60000); // Wait for 1 minute before making the next request
+  delay(10000); // Wait for 1 minute before making the next request
 }
--- server.js
+++ server.js
@@ -5,79 +5,23 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 
-app.get("/results", (req, res) => {
-  const weatherData = {
-    temp: 59,
-    wind: 3,
-    condition: "Clouds",
-    sunrise: "06:12 AM",
-    sunset: "07:30 PM",
-    loc: "Torrance",
-    icon: "https://openweathermap.org/img/wn/04n@2x.png",
+app.get("/", async (req, res) => {
+  const axios = require("axios");
+
+  const options = {
+    method: "GET",
+    url: "https://catfact.ninja/fact",
   };
 
-  res.json(weatherData); // Return weather data as JSON
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    res.json(response.data.fact);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(3000, () => {
   console.log("Listening on Port 3000");
 });
-
-const fetchWeatherData = async (city, state, apiKey) => {
-  try {
-    //fetching the GeoLocater to get Coordinates
-    const geoRes = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=5&appid=${apiKey}`
-    );
-    const geoData = await geoRes.json();
-    const latitutde = geoData[0].lat.toFixed(2);
-    const longitude = geoData[0].lon.toFixed(2);
-
-    //fetching the WeatherAPI
-    const weatherResponse = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitutde}&lon=${longitude}&tz=-08:00&units=Imperial&appid=${apiKey}`
-    );
-    const weatherData = await weatherResponse.json();
-    const iconCode = weatherData.weather[0].icon;
-
-    const temp = Math.floor(weatherData.main.temp);
-    const loc = weatherData.name;
-    const icon = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-
-    const condition = weatherData.weather[0].main;
-    const wind = Math.floor(weatherData.wind.speed);
-    const sunriseUtc = Math.floor(weatherData.sys.sunrise);
-    const sunsetUtc = Math.floor(weatherData.sys.sunset);
-
-    //Timezone converted from UTC in seconds to whatever time zone the user inputs
-    const timeZoneDif = weatherData.timezone;
-    const sunrise = convertTime(sunriseUtc, timeZoneDif);
-    const sunset = convertTime(sunsetUtc, timeZoneDif);
-
-    return { temp, wind, condition, sunrise, sunset, loc, icon };
-  } catch (e) {
-    console.log("there has been an error ", e);
-  }
-};
-
-const convertTime = (t, tD) => {
-  const utcTimeInSeconds = t + tD;
-
-  const date = new Date(utcTimeInSeconds * 1000);
-
-  // Extract individual time components
-  let amHours = date.getUTCHours();
-  const amMinutes = date.getUTCMinutes();
-  const meridiem = amHours >= 12 ? "PM" : "AM";
-
-  // Convert hours to 12-hour format
-  amHours = amHours % 12;
-  amHours = amHours ? amHours : 12;
-
-  // Format the time
-  const formattedTime = `${amHours.toString().padStart(2, "0")}:${amMinutes
-    .toString()
-    .padStart(2, "0")} ${meridiem}`;
-
-  return formattedTime;
-};
